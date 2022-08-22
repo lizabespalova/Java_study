14ad8b16614f2a0bcd4dcee7c40ff87186ac27ed
lizabespalova <bespalovalizv@gmail.com>
2022-08-23 00:23:37 +0300

    try to make git

---
+++ src/main/java/com/javacourse/se/lesson1/Lesson1.java
@@ -0,0 +1,7 @@
+package com.javacourse.se.lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/com/javacourse/se/lesson2/Lesson2.java
@@ -0,0 +1,7 @@
+package com.javacourse.se.lesson2;
+
+public class Lesson2 {
+    public static void main(String[] args){
+        System.out.println("Hello Lisa");
+    }
+}
